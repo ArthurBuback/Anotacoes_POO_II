ff3185403d8c6b97398b90eae093c4667d3de46a
ArthurBuback <103044946+ArthurBuback@users.noreply.github.com>
2023-11-10 08:39:36 -0300

    Add files via upload

---
+++ armazenar_2/src/services/UserServices.ts
@@ -0,0 +1,70 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class UserService{
+    constructor(){}
+
+    async createUser(user: Prisma.UserCreateInput){
+        try{
+            const newuser = await prisma.user.create({
+                data: user
+            });
+            return newuser;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
+
+    async findUsers(id: string | undefined){
+        try{
+            if(id){
+                const user = await prisma.user.findUnique({
+                    where: {
+                        id
+                    }
+                })
+                return user;
+            }else{
+                const users = await prisma.user.findMany();
+                return users;
+            }
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
+
+    async updateUsers(id: string, newData: Prisma.UserCreateInput){
+        try{
+            const UserUpdated = await prisma.user.update({
+                where: {
+                    id
+                }, data: {
+                    email: newData.email,
+                    name: newData.name,
+                }
+            });
+            return UserUpdated;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
+
+    async deleteUser(id: string){
+        try{
+            if(!id){
+                return console.log("Um id é necessário para deletar um usuário");
+            }
+            await prisma.user.delete({where: {id}});
+            return "ok";
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
+}
+
+export default new UserService();
